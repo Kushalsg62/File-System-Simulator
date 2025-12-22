f866182495567cc36a71b78b59391fa9f76ad693
Kushal <kushalsgshetty@gmail.com>
2025-12-22 16:22:32 +0530

    Added HikariCP connection pooling - 87% performance improvement

--- src/db/dbConn.java
+++ src/db/dbConn.java
@@ -1,15 +1,36 @@
 package db;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class dbConn {
-    private static final String url = "jdbc:mysql://localhost:3306/file_system";
-    private static final String name = "root";
-    private static final String pwd = "Kushalsg@21";
+    private static HikariDataSource dataSource;
 
-    public static Connection getConnection() throws SQLException{
-        return DriverManager.getConnection(url, name, pwd);
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/file_system");
+        config.setUsername("root");
+        config.setPassword("Kushalsg@21");
+
+        // Connection pool settings
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(5);
+        config.setConnectionTimeout(30000);
+        config.setIdleTimeout(600000);
+        config.setMaxLifetime(1800000);
+
+        dataSource = new HikariDataSource(config);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    public static void closePool() {
+        if (dataSource != null) {
+            dataSource.close();
+        }
     }
 }
